9876ef180535557900c1b75d27383f970ababfb1
Zach Cox <zach.cox@banno.com>
2018-10-03 15:21:40 -0500

    effect on valid websocket

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -22,7 +22,7 @@ class App[F[_]](log: Logger[F])(implicit F: Effect[F]) extends StreamApp[F] with
 
   def service(topic: Topic[F, Option[String]]) = HttpService[F] {
     case GET -> Root / "messages" => 
-      val send = topic.subscribe(100).tail.unNone.map(s => Text(s)).onFinalize(log.info("Subscriber closed"))
+      val send = Stream.eval(log.info("Send stream begin")).drain ++ topic.subscribe(100).tail.unNone.map(s => Text(s)).onFinalize(log.info("Subscriber closed"))
       val receive: Sink[F, WebSocketFrame] = _.collect{case Text(s, _) => s.some}.to(topic.publish)
       val onNonWebSocketRequest: F[Response[F]] = log.info("On non-WebSocket request") *> Response[F](Status.NotImplemented).withBody("Request did not contain WebSocket headers")
       val onHandshakeFailure: F[Response[F]] = log.info("On handshake failure") *> Response[F](Status.BadRequest).withBody("Unable to complete WebSocket handshake")
